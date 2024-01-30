504c4ee13de05a05f70236111d18d1c2119e057b
malikmtayyab <tayyabshahid597@gmail.com>
2024-01-31 01:07:53 +0500

    Code merged

--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -2,5 +2,6 @@ export default {
     getAllFarms: 'tankFarms',
     addTankFarms: 'tankFarms',
     getAllTanks: 'tanks',
-    addTank: 'tanks'
+    addTank: 'tanks',
+    getDashboardData: 'tanks/dashboard'
 }
